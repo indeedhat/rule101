1be97bde1478d14264e5f863dc8cb10d237a3df3
indeedhat <iam4423@gmail.com>
2022-01-19 00:03:35 +0000

    refactor: dont bother with the whole grid

--- main.go
+++ main.go
@@ -24,35 +24,27 @@ func (gr GridRow) String() string {
 }
 
 func main() {
-	grid := []GridRow{}
+	prevRow := make(GridRow, GridSize)
+	prevRow[GridSize-2] = 1
+	fmt.Println(prevRow)
 
-	for row := 0; row < GridSize; row++ {
-		grid = append(grid, GridRow{})
+	for row := 1; row < GridSize; row++ {
+		currentRow := GridRow{}
 
 		for col := 0; col < GridSize; col++ {
-			if row == 0 && col == GridSize-2 {
-				grid[row] = append(grid[row], 1)
-				continue
-			}
-
-			if row == 0 {
-				grid[row] = append(grid[row], 0)
-				continue
-			}
-
-			mask := grid[row-1][max(0, col-1)] << 2
-			mask |= grid[row-1][col] << 1
-			mask |= grid[row-1][min(GridSize-1, col+1)] << 0
+			mask := prevRow[max(0, col-1)] << 2
+			mask |= prevRow[col] << 1
+			mask |= prevRow[min(GridSize-1, col+1)] << 0
 
 			if mask == 7 || mask == 4 || mask == 0 {
-				grid[row] = append(grid[row], 0)
+				currentRow = append(currentRow, 0)
 			} else {
-				grid[row] = append(grid[row], 1)
+				currentRow = append(currentRow, 1)
 			}
 		}
-		fmt.Println(grid[row])
+		fmt.Println(currentRow)
+		prevRow = currentRow
 	}
-
 }
 
 func min(a, b int) int {
